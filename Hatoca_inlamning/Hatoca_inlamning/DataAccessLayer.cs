79cc78baa5fb3b2943412b619c4fd61bfd82c911
ha2502wa-s <35835134+ha2502wa-s@users.noreply.github.com>
2018-02-15 13:08:57 +0200

    Added classes

---
+++ Hatoca_inlamning/Hatoca_inlamning/DataAccessLayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hatoca_inlamning
+{
+	class DataAccessLayer
+	{
+		public string hej = "hej";
+	}
+}
