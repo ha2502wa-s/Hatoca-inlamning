eff6e1acd19f6e30b362484ec7eefe8e0570d047
tovehultin <32096517+tovehultin@users.noreply.github.com>
2018-02-15 12:24:33 +0100

    lagt till queries

---
+++ Hatoca_inlamning/Hatoca_inlamning/DAL/Queries.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hatoca_inlamning.DAL
+{
+    class Queries
+    {
+        public String GetCustomer()
+        {
+            return "SELECT * FROM Customer WHERE cpnr = ? ";
+        }
+
+        public String GetCustomerToTable()
+        {
+            return "SELECT cpnr AS 'Customer pnr' UPPER (cname) AS 'Name', " + "UPPER(caddress) AS 'Address' FROM Customer WHERE cpnr = ?";
+        }
+
+        public String GetGymsession()
+        {
+            return "SELECT * FROM Gymsession WHERE gid = ?";
+        }
+
+        public String GetInstructor()
+        {
+            return "SELECT * FROM Instructor WHERE iid = ?";
+        }
+
+        public String AddCustomer()
+        {
+            return "INSERT INTO Customer VALUES (?,?,?,?,?)";
+        }
+
+        public String AddGymsession()
+        {
+            return "INSERT INTO Gymsession VALUES (?,?,?,?,?)";
+        }
+
+        public String DeleteGymsession()
+        {
+            return "DELETE FROM Gymsession WHERE gid = ?";
+        }
+        public String DeleteCustomer()
+        {
+            return "DELETE FROM Customer WHERE cpnr = ?";
+        }
+        public String BookSession()
+        {
+            return "INSERT INTO book VALUES (?,?)";
+        }
+        public String AllSessions()
+        {
+            return "SELECT UPPER (gid) AS Session ID, UPPER (gTitle) AS Title, UPPER (gPlace) AS Place, UPPER (gTime) AS Time, UPPER (gmax_spots) as Spots FROM Session";
+        }
+    }
+}
