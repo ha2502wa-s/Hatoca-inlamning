fe4467c10f2c7c691047b4d0de2fe217c93becff
ha2502wa-s <35835134+ha2502wa-s@users.noreply.github.com>
2018-02-15 13:18:21 +0200

    Added windows form

---
+++ Hatoca_inlamning/Hatoca_inlamning/Application.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Hatoca_inlamning
+{
+	public partial class Application : Form
+	{
+		public Application()
+		{
+			InitializeComponent();
+		}
+	}
+}
