933a50b6affcdb6fc9eef4e21781f1d290d038e2
tovehultin <32096517+tovehultin@users.noreply.github.com>
2018-02-16 12:36:06 +0100

    Connection och controller

--- Hatoca_inlamning/Hatoca_inlamning/Controller/Controller.cs
+++ Hatoca_inlamning/Hatoca_inlamning/Controller/Controller.cs
@@ -5,7 +5,34 @@ using System.Text;
 
 namespace Hatoca_inlamning
 {
-	class Controller
-	{
+    class Controller
+    {
+       /* private DataAccessLayer dal = new DataAccessLayer();
+
+        public Customer GetCustomer(cpnr)
+        {return dal.GetCustomer(cpnr); }
+       
+        public Gymsession GetGymsession(gid)
+        {return dal.GetGymsession(gid);}
+
+        public void DeleteCustomer(cpnr)
+        {dal.DeleteCustomer(cpnr);}
+
+        public void DeleteGymsession (gid)
+        { dal.DeleteGymsession(gid); }
+
+        public void AddCustomer (cpnr)
+        { dal.AddCustomer(cpnr);}
+
+        public void AddGymSession (gid)
+        { dal.AddGymsession(gid); }
+        
+        public void DeleteBook (String cpnr, String gid)
+        { dal.DeleteBook(cpnr, gid); }
+        
+        public void AddBook (String cpnr, String gid)
+        {dal.AddBook(cpnr, gid) ; }*/
+        
+
 	}
 }
